a37a828a48618f1d31816e4eb3862d957774e3d0
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-13 17:54:03 +0100

    Fixes PHPCS

--- src/Shell/Task/SymlinkTask.php
+++ src/Shell/Task/SymlinkTask.php
@@ -4,24 +4,34 @@ namespace App\Shell\Task;
 use Cake\Console\Shell;
 
 class SymlinkTask extends Shell {
-    public function main() {
 
-    }
+/**
+ * create() will create a symbolic link unless it already exists
+ *
+ * @param string $target containing full path to an existing file
+ * @param string $link containing full path of symbolic link to be created
+ * @return void
+ */
+	public function create($target, $link) {
+		$this->out("Creating symbolic link $link");
+		if ($this->exists($link)) {
+			$this->out("* Skipping: symlink already exist");
+			return;
+		}
+		symlink($target, $link);
+	}
 
-    public function create($target, $link) {
-        $this->out("Creating symbolic link $link");
-        if ($this->exists($link)){
-            $this->out("* Skipping: symlink already exist");
-            return;
-        }
-        symlink($target, $link);
-    }
-
-    public function exists($link) {
-        if (is_link($link)){
-            return true;
-        }
-        return false;
-    }
+/**
+ * exists() checks if a symbolic link exists
+ *
+ * @param string $link containing full path to the symbolic link to check
+ * @return bool true when the symbolic link exist
+ */
+	public function exists($link) {
+		if (is_link($link)) {
+			return true;
+		}
+		return false;
+	}
 
 }
