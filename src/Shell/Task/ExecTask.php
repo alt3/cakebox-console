f2db0726e43ecf42c30267ba44a827d61159e29f
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-13 17:42:44 +0100

    Fixes PHPCS

--- src/Shell/Task/ExecTask.php
+++ src/Shell/Task/ExecTask.php
@@ -4,22 +4,25 @@ namespace App\Shell\Task;
 use Cake\Console\Shell;
 
 class ExecTask extends Shell {
-    public function main() {
 
-    }
+/**
+ * run() executes a command as either root or vagrant user.
+ *
+ * @param string $command containing full path and connabd arguments, options
+ * @param bool $su set to false to run as root, true to run as vagrant user
+ * @return int $err containing exit code of executed command
+ */
+	public function run($command, $su = false) {
+		if ($su == false) {
+			$ret = $this->out(exec("$command", $out, $err));
+		} else {
+			$ret = exec("su vagrant -c \"$command\"", $out, $err);
+		}
 
-    public function run($command, $su = false) {
-
-        if ($su == false){
-            $ret = $this->out(exec("$command", $out, $err));
-        }else{
-            $ret = exec("su vagrant -c \"$command\"", $out, $err);
-        }
-
-        if($err){
-            $this->out("Error executing command '$command'");
-            exit(1);
-        }
-    }
+		if ($err) {
+			$this->out("Error executing command '$command'");
+			exit(1);
+		}
+	}
 
 }
