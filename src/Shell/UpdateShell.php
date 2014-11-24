3e819b29978dd0e301020ceb879032f8113e54b0
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-24 22:00:18 +0100

    Adds UpdateShell

---
+++ src/Shell/UpdateShell.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Shell;
+
+use Cake\Console\Shell;
+
+/**
+* Shell class for self-updating cakebox console and management website.
+*/
+class UpdateShell extends Shell {
+
+/**
+ * @var array Shell Tasks used by this shell.
+ */
+	public $tasks = [
+		'Exec'
+	];
+	
+/**
+ * Update cakebox-console repository and run composer update.
+ *
+ * @return bool
+ */
+	public function main() {
+		$this->out("Updating cakebox console and management website");
+
+		# Git pull cakebox-console
+		$this->out("* Updating repository");
+		if ($this->Exec->runCommand("cd /cakebox/console; git fetch; git reset --hard origin/master", 'vagrant')) {
+			$this->out("Error git pulling cakebox-console");
+		}
+
+		# Composer update cakebox-console
+		$this->out("* Updating composer");
+		if ($this->Exec->runCommand("cd /cakebox/console; composer update --prefer-dist --no-dev", 'vagrant')) {
+			$this->out("Error composer updating");
+		}
+	}
+
+}
