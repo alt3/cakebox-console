411063fedd6763bdc2ebaf2c32ed2f5942842ad3
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-12-07 22:09:17 +0100

    Fixes columnize on matching dimension

--- src/View/Helper/CakeboxHelper.php
+++ src/View/Helper/CakeboxHelper.php
@@ -6,13 +6,16 @@ use Cake\View\Helper;
 class CakeboxHelper extends Helper {
 
 /**
- * Chop up an array into muliple, evenly divided, column arrays
+ * Chop up an array into evenly divided column-arrays
  *
  * @param array Full array
  * @param int Number of columns to divide data into
  * @return array Array with extra root-id containing column elements
  */
 	public function columnize($data, $columns){
+		if (count($data) == $columns) {
+			return [$data];
+		}
 		return array_chunk($data, ceil(count($data) / $columns));
 	}
 
