f0fb94c95a875ab899329c83266f311c7e3ad2eb
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-12-16 22:25:49 +0100

    Changes default database credentials

--- src/Shell/DatabaseShell.php
+++ src/Shell/DatabaseShell.php
@@ -46,8 +46,8 @@ class DatabaseShell extends AppShell {
 					'name' => ['help' => __('Name to be used for the databases.'), 'required' => true]
 				],
 				'options' => [
-					'username' => ['short' => 'u', 'help' => __('Username with localhost database access.'), 'default' => 'cakebox'],
-					'password' => ['short' => 'p', 'help' => __('Password for user with localhost access.'), 'default' => 'secret'],
+					'username' => ['short' => 'u', 'help' => __('Username with localhost database access.'), 'default' => 'username'],
+					'password' => ['short' => 'p', 'help' => __('Password for user with localhost access.'), 'default' => 'password'],
 					'force' => ['short' => 'f', 'help' => __('Drop existing database.'), 'boolean' => true]
 				]
 		]]);
