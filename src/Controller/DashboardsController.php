23534fc33ea42058ccab4340355608dadc050460
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-12-16 09:47:17 +0100

    Removes redundant viewVar

--- src/Controller/DashboardsController.php
+++ src/Controller/DashboardsController.php
@@ -29,11 +29,6 @@ class DashboardsController extends AppController {
                 'databases' => $this->cbi->getDatabaseCount(),
                 'sites' => $this->cbi->getNginxFileCount()
             ],
-            'checks' => [
-                'system' => $this->cbc->getSystemChecks(),
-                'application' => $this->cbc->getApplicationChecks('/cakebox/console'),
-                'security' =>$this->cbc->getSecurityChecks()
-            ],
             'operating_system' => $this->cbi->getOperatingSystem(),
             'packages' => $this->cbi->getPackages(),
             'php_modules' => $this->cbi->getPhpModules(),
