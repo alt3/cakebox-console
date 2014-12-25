4b7d95a15be000bcf5fcd863311c31fe5bfe25e0
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-12-25 15:52:52 +0100

    Adds Monolog loggers for app and cli

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,36 +1,36 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         0.10.8
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
+* CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+* Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+*
+* Licensed under The MIT License
+* For full copyright and license information, please see the LICENSE.txt
+* Redistributions of files must retain the above copyright notice.
+*
+* @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+* @link          http://cakephp.org CakePHP(tm) Project
+* @since         0.10.8
+* @license       http://www.opensource.org/licenses/mit-license.php MIT License
+*/
 
 /**
- * Configure paths required to find CakePHP + general filepath
- * constants
- */
+* Configure paths required to find CakePHP + general filepath
+* constants
+*/
 require __DIR__ . '/paths.php';
 
 // Use composer to load the autoloader.
 require ROOT . DS . 'vendor' . DS . 'autoload.php';
 
 /**
- * Bootstrap CakePHP.
- *
- * Does the various bits of setup that CakePHP needs to do.
- * This includes:
- *
- * - Registering the CakePHP autoloader.
- * - Setting the default application paths.
- */
+* Bootstrap CakePHP.
+*
+* Does the various bits of setup that CakePHP needs to do.
+* This includes:
+*
+* - Registering the CakePHP autoloader.
+* - Setting the default application paths.
+*/
 require CORE_PATH . 'config' . DS . 'bootstrap.php';
 
 use Cake\Cache\Cache;
@@ -51,13 +51,13 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 /**
- * Read configuration file and inject configuration into various
- * CakePHP classes.
- *
- * By default there is only one configuration file. It is often a good
- * idea to create multiple configuration files, and separate the configuration
- * that changes from configuration that does not. This makes deployment simpler.
- */
+* Read configuration file and inject configuration into various
+* CakePHP classes.
+*
+* By default there is only one configuration file. It is often a good
+* idea to create multiple configuration files, and separate the configuration
+* that changes from configuration that does not. This makes deployment simpler.
+*/
 try {
 	Configure::config('default', new PhpConfig());
 	Configure::load('app', 'default', false);
@@ -79,25 +79,25 @@ if (!Configure::read('debug')) {
 }
 
 /**
- * Set server timezone to UTC. You can change it to another timezone of your
- * choice but using UTC makes time calculations / conversions easier.
- */
+* Set server timezone to UTC. You can change it to another timezone of your
+* choice but using UTC makes time calculations / conversions easier.
+*/
 date_default_timezone_set('UTC');
 
 /**
- * Configure the mbstring extension to use the correct encoding.
- */
+* Configure the mbstring extension to use the correct encoding.
+*/
 mb_internal_encoding(Configure::read('App.encoding'));
 
 /**
- * Set the default locale. This controls how dates, number and currency is
- * formatted and sets the default language to use for translations.
- */
+* Set the default locale. This controls how dates, number and currency is
+* formatted and sets the default language to use for translations.
+*/
 ini_set('intl.default_locale', 'en_US');
 
 /**
- * Register application error and exception handlers.
- */
+* Register application error and exception handlers.
+*/
 $isCli = php_sapi_name() === 'cli';
 if ($isCli) {
 	(new ConsoleErrorHandler(Configure::consume('Error')))->register();
@@ -111,11 +111,11 @@ if ($isCli) {
 }
 
 /**
- * Set the full base URL.
- * This URL is used as the base of all absolute links.
- *
- * If you define fullBaseUrl in your config file you can remove this.
- */
+* Set the full base URL.
+* This URL is used as the base of all absolute links.
+*
+* If you define fullBaseUrl in your config file you can remove this.
+*/
 if (!Configure::read('App.fullBaseUrl')) {
 	$s = null;
 	if (env('HTTPS')) {
@@ -137,8 +137,8 @@ Log::config(Configure::consume('Log'));
 Security::salt(Configure::consume('Security.salt'));
 
 /**
- * Setup detectors for mobile and tablet.
- */
+* Setup detectors for mobile and tablet.
+*/
 Request::addDetector('mobile', function ($request) {
 	$detector = new \Detection\MobileDetect();
 	return $detector->isMobile();
@@ -149,39 +149,51 @@ Request::addDetector('tablet', function ($request) {
 });
 
 /**
- * Custom Inflector rules, can be set to correctly pluralize or singularize table, model, controller names or whatever other
- * string is passed to the inflection functions
- *
- * Inflector::rules('plural', ['/^(inflect)or$/i' => '\1ables']);
- * Inflector::rules('irregular' => ['red' => 'redlings']);
- * Inflector::rules('uninflected', ['dontinflectme']);
- * Inflector::rules('transliteration', ['/å/' => 'aa']);
- */
+* Custom Inflector rules, can be set to correctly pluralize or singularize table, model, controller names or whatever other
+* string is passed to the inflection functions
+*
+* Inflector::rules('plural', ['/^(inflect)or$/i' => '\1ables']);
+* Inflector::rules('irregular' => ['red' => 'redlings']);
+* Inflector::rules('uninflected', ['dontinflectme']);
+* Inflector::rules('transliteration', ['/å/' => 'aa']);
+*/
 
 /**
- * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
- * Uncomment one of the lines below, as you need. make sure you read the documentation on Plugin to use more
- * advanced ways of loading plugins
- *
- * Plugin::loadAll(); // Loads all plugins at once
- * Plugin::load('DebugKit'); //Loads a single plugin named DebugKit
- *
- */
-#Plugin::load('DebugKit', ['bootstrap' => true]);
+* Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
+* Uncomment one of the lines below, as you need. make sure you read the documentation on Plugin to use more
+* advanced ways of loading plugins
+*
+* Plugin::loadAll(); // Loads all plugins at once
+* Plugin::load('DebugKit'); //Loads a single plugin named DebugKit
+*
+*/
+
+// Only try to load DebugKit in development mode
+// Debug Kit should not be installed on a production system
+if (Configure::read('debug')) {
+	Plugin::load('DebugKit', ['bootstrap' => true]);
+}
 
 /**
- * Connect middleware/dispatcher filters.
- */
-
+* Connect middleware/dispatcher filters.
+*/
 DispatcherFactory::add('Asset');
 DispatcherFactory::add('Routing');
 DispatcherFactory::add('ControllerFactory');
 
 /**
- * Cakebox: use Monolog for all logging to enable Logstash > Elasticsearch forwarding.
- */
-Log::config('default', function () {
-	$log = new Logger('app');
-	$log->pushHandler(new StreamHandler('/cakebox/console/logs/cakebox.log'));
-	return $log;
-});
+* Cakebox: use Monolog to create a combined log (with all log levels) to enable
+* Logstash > Elasticsearch forwarding, unless we are in CLI mode which defines
+* it's own Monlog logger (with 'cli' prefix tag instead of 'app').
+*/
+if (!$isCli) {
+	Log::config('default-app', function () {
+		$log = new Logger('app.cakebox');
+		$log->pushHandler(new StreamHandler('/cakebox/console/logs/cakebox.log'));
+		return $log;
+	});
+
+	// Stop using the now redundant default CakePHP file loggers
+	Log::drop('debug');
+	Log::drop('error');
+}
--- config/bootstrap_cli.php
+++ config/bootstrap_cli.php
@@ -1,27 +1,52 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         3.0.0
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
+* CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+* Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+*
+* Licensed under The MIT License
+* For full copyright and license information, please see the LICENSE.txt
+* Redistributions of files must retain the above copyright notice.
+*
+* @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+* @link          http://cakephp.org CakePHP(tm) Project
+* @since         3.0.0
+* @license       http://www.opensource.org/licenses/mit-license.php MIT License
+*/
 use Cake\Core\Configure;
+use Cake\Core\Exception\MissingPluginException;
+use Cake\Core\Plugin;
+use Cake\Log\Log;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 /**
- * Additional bootstrapping and configuration for CLI environments should
- * be put here.
+* Additional bootstrapping and configuration for CLI environments should
+* be put here.
+*/
+try {
+	Plugin::load('Bake');
+} catch (MissingPluginException $e) {
+	// Do not halt if the plugin is missing
+}
+
+/**
+ * Cakebox: use Monolog to create a combined log (with all log levels) to enable
+ * Logstash > Elasticsearch forwarding. This logger is different from the one in
+ * bootstrap.php in that is uses the 'cli' prefix tag instead of 'app'.
+*/
+Log::config('default', function () {
+	$log = new Logger('cli.cakebox');
+	$log->pushHandler(new StreamHandler('/cakebox/console/logs/cakebox.log'));
+	return $log;
+});
+
+/**
+ * Stop using the now redundant default CakePHP file loggers.
+ *
+ * Note: does not work yet because of consume() line 134 in bootstrap.php.
+ * Current workaround is deleting the Log. configurations
  */
-
-// Set logs to different files so they don't have permission conflicts.
-#Configure::write('Log.debug.file', 'cakebox.log');
-#Configure::write('Log.error.file', 'cakebox.err');
-
-Configure::write('Log.debug.file', 'cakebox.log');
-Configure::write('Log.error.file', 'cakebox.log');
+Log::drop('debug');
+Log::drop('error');
+#Configure::delete('Log.error');
+#Configure::delete('Log.error');
