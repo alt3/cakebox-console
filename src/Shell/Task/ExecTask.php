18b387f37d557ba8dadcc8b1da4108b351179bee
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-12-27 00:02:53 +0100

    PSR2 shell tasks

--- src/Shell/Task/ExecTask.php
+++ src/Shell/Task/ExecTask.php
@@ -6,61 +6,64 @@ use Cake\Console\Shell;
 /**
  * Task class for managing system command shelling.
  */
-class ExecTask extends Shell {
+class ExecTask extends Shell
+{
 
-/**
- * Execute a system command as root or using su with provided username.
- *
- * @param string $command Full path to the command with options and arguments
- * @param string $username Optional sudo user used to execute the command
- * @return int $err Exit code of executed command
- */
-	public function runCommand($command, $username = "root") {
-		$this->out("Executing system command as $username", 1, Shell::VERBOSE);
-		if ($username == "root") {
-			$command = "$command 2>&1";
-		} else {
-			$command = "su $username -c \"$command\" 2>&1";
-		}
-		$this->out(" => $command", 1, Shell::VERBOSE);
+    /**
+     * Execute a system command as root or using su with provided username.
+     *
+     * @param string $command Full path to the command with options and arguments.
+     * @param string $username Optional sudo user used to execute the command.
+     * @return int $err Exit code of executed command
+     */
+    public function runCommand($command, $username = "root")
+    {
+        $this->out("Executing system command as $username", 1, Shell::VERBOSE);
+        if ($username == "root") {
+            $command = "$command 2>&1";
+        } else {
+            $command = "su $username -c \"$command\" 2>&1";
+        }
+        $this->out(" => $command", 1, Shell::VERBOSE);
 
-		# Execute the command, capture exit code, stdout and stderr
-		$ret = exec($command, $stdout, $exitCode);
-		foreach ($stdout as $line) {
-			if (!empty($line)) {
-				$this->out(" => $line", 1, Shell::VERBOSE);
-			}
-		}
+     # Execute the command, capture exit code, stdout and stderr
+        $ret = exec($command, $stdout, $exitCode);
+        foreach ($stdout as $line) {
+            if (!empty($line)) {
+                $this->out(" => $line", 1, Shell::VERBOSE);
+            }
+        }
 
-		# Return exit-code and write stdout to log if errors occured
-		if ($exitCode) {
-			$this->out("Error: Non-zero exit code ($exitCode)");
-			foreach ($stdout as $line) {
-				if (!empty($line)) {
-					$this->out(" => $line");
-				}
-			}
-			return $exitCode;
-		}
-		return false;
-	}
+     # Return exit-code and write stdout to log if errors occured
+        if ($exitCode) {
+            $this->out("Error: Non-zero exit code ($exitCode)");
+            foreach ($stdout as $line) {
+                if (!empty($line)) {
+                    $this->out(" => $line");
+                }
+            }
+            return $exitCode;
+        }
+        return false;
+    }
 
-/**
- * Exit PHP script with exit code 0 to inform bash about success.
- *
- * @return void
- */
-	public function exitBashSuccess() {
-		exit (0);
-	}
+    /**
+     * Exit PHP script with exit code 0 to inform bash about success.
+     *
+     * @return void
+     */
+    public function exitBashSuccess()
+    {
+        exit (0);
+    }
 
-/**
- * Exit PHP script with exit code 0 to inform bash about success.
- *
- * @return void
- */
-	public function exitBashError() {
-		exit (1);
-	}
-
+    /**
+     * Exit PHP script with exit code 0 to inform bash about success.
+     *
+     * @return void
+     */
+    public function exitBashError()
+    {
+        exit (1);
+    }
 }
--- src/Shell/Task/SymlinkTask.php
+++ src/Shell/Task/SymlinkTask.php
@@ -6,35 +6,37 @@ use Cake\Console\Shell;
 /**
  * Task class for managing symbolic links.
  */
-class SymlinkTask extends Shell {
+class SymlinkTask extends Shell
+{
 
-/**
- * Create a symbolic link unless it already exists.
- *
- * @param string $target Full path to the existing file
- * @param string $link Full path where symbolic link will be created
- * @return void
- */
-	public function create($target, $link) {
-		$this->out("Creating symbolic link $link");
-		if ($this->exists($link)) {
-			$this->out("* Skipping: symlink already exist");
-			return;
-		}
-		symlink($target, $link);
-	}
+    /**
+     * Create a symbolic link unless it already exists.
+     *
+     * @param string $target Full path to the existing file.
+     * @param string $link Full path where symbolic link will be created.
+     * @return void
+     */
+    public function create($target, $link)
+    {
+        $this->out("Creating symbolic link $link");
+        if ($this->exists($link)) {
+            $this->out("* Skipping: symlink already exist");
+            return;
+        }
+        symlink($target, $link);
+    }
 
-/**
- * Check if a symbolic link already exists.
- *
- * @param string $link Full path to the file/link to check
- * @return bool
- */
-	public function exists($link) {
-		if (is_link($link)) {
-			return true;
-		}
-		return false;
-	}
-
+    /**
+     * Check if a symbolic link already exists.
+     *
+     * @param string $link Full path to the file/link to check.
+     * @return bool
+     */
+    public function exists($link)
+    {
+        if (is_link($link)) {
+            return true;
+        }
+        return false;
+    }
 }
