dd6e8bae9f5fca7bb63de0ddd71e103615eb6e01
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-12-28 16:54:11 +0100

    Disables Debugkit

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -172,7 +172,7 @@ Request::addDetector('tablet', function ($request) {
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
-	Plugin::load('DebugKit', ['bootstrap' => true]);
+//	Plugin::load('DebugKit', ['bootstrap' => true]);
 }
 
 /**
