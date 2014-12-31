e677aa0bcedecf8ce10470d3b762e6002c2584c1
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-12-31 14:32:35 +0100

    Enables custom console screen i/o

--- src/Shell/AppShell.php
+++ src/Shell/AppShell.php
@@ -3,12 +3,18 @@ namespace App\Shell;
 
 use Cake\Console\Shell;
 use Cake\Core\Configure;
+use Cake\Log\Log;
 
 /**
  * Application Shell class extended by all cakebox-console shells.
  */
 class AppShell extends Shell
 {
+
+    public function initialize() {
+        $this->_io->setLoggers(false);
+        parent::initialize();
+    }
 
     /**
      * Override Cake\Console\Shell method to return different welcome screen.
@@ -21,4 +27,49 @@ class AppShell extends Shell
         $this->out(sprintf('<info>CakePHP %s Console</info>', 'v' . Configure::version()));
         $this->hr();
     }
+
+    /**
+     * Only output message to screen when script is run with --verbose argument
+     *
+     * @param string $message
+     * @return void
+     */
+    public function debug($message) {
+        log::debug($message);
+        $this->out($message, 1, Shell::VERBOSE);
+    }
+
+    /*
+     * Always output info message to screen (even when using --quiet)
+     *
+     * @param string $message
+     * @return void
+     */
+    public function info($message) {
+        log::info($message);
+        $this->out($message, 1, Shell::QUIET);
+    }
+
+    /*
+    * Always output warning message to screen (even when using --quiet)
+    *
+    * @param string $message
+    * @return void
+    */
+    public function warning($message) {
+        log::warning($message);
+        $this->out($message, 1, Shell::QUIET);
+    }
+
+    /*
+    * Always output warning message to screen (even when using --quiet)
+    *
+    * @param string $message
+    * @return void
+    */
+    public function fatal($message) {
+        log::warning($message);
+        $this->out($message, 1, Shell::QUIET);
+    }
+
 }
