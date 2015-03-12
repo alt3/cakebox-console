78cadc0a2a83e6013d1f3ce5002eabcd7077cc0a
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-03-12 17:45:43 +0000

    Fixes broken sitefiles ajax loading

--- webroot/js/pages/site_files.js
+++ webroot/js/pages/site_files.js
@@ -26,7 +26,7 @@ $( document ).ready(function() {
 		var button = $(event.relatedTarget) // Button that triggered the modal
 		var filename = button.closest('tr').find('td.filename').html()
 		$('.modal-title').html('/etc/nginx/sites-available/' + filename)
-		var jqxhr = $.getJSON( 'site_files/file/' + filename + '.json', function(data) {
+		var jqxhr = $.getJSON( 'sitefiles/file/' + filename + '.json', function(data) {
 			console.log(modal)
 			modal.find('.modal-body').html('<pre>' + data.content + '</pre>')
 		})
