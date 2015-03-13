60bbaeb1ae110df218d6f63536cb433c5c8a44b9
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-03-13 12:11:29 +0000

    Adds dashboard subcommand to ConfigShell to switch between http/https

--- src/Shell/ConfigShell.php
+++ src/Shell/ConfigShell.php
@@ -30,6 +30,18 @@ class ConfigShell extends AppShell
                 ]
             ]
         ]);
+
+        $parser->addSubcommand('dashboard', [
+            'parser' => [
+                'description' => [
+                __("Configures the Cakebox Dashboard.")
+                ],
+                'options' => [
+                    'protocol' => ['help' => __('Git user.name to use globally.'), 'required' => false, 'options' => ['http', 'https']]
+                ]
+            ]
+        ]);
+
         return $parser;
     }
 
@@ -62,5 +74,43 @@ class ConfigShell extends AppShell
             }
         }
         $this->exitBashSuccess("Git configuration updated successfully");
+    }
+
+    /**
+     * Displays the protocol currently used by the Dashboard website. Changes
+     * the protocol to HTTP/HTTPS when using the --protocol option.
+     *
+     * @todo show new url after completion (should use yaml for host and ip)
+     *
+     * @return void
+     */
+    public function dashboard()
+    {
+        // no --protocol parameter given, display current protocol
+        if (!isset($this->params['protocol'])) {
+            if ($this->cbi->dashboardUsesHttps()) {
+                $this->exitBashSuccess("The Cakebox Dashboard is using HTTPS");
+            }
+            $this->exitBashSuccess("The Cakebox Dashboard is using HTTP");
+        }
+        $protocol = $this->params['protocol'];
+
+        $this->logStart("Changing Cakebox Dashboard protocol to " . $protocol);
+
+        // do not change if protocol is already active
+        if ($protocol === 'https' & $this->cbi->dashboardUsesHttps()) {
+            $this->logInfo("* Skipping: website already uses HTTPS");
+            $this->exitBashSuccess("Command completed successfully");
+        }
+        if ($protocol === 'http' & !$this->cbi->dashboardUsesHttps()) {
+            $this->logInfo("* Skipping: website already uses HTTP");
+            $this->exitBashSuccess("Command completed successfully");
+        }
+
+        // enable new protocol by replacing Ngixnx vhost configuration file
+        if (!$this->execute->setDashboardProtocol($protocol)) {
+            $this->exitBashError("Error changing protocol.");
+        }
+        $this->exitBashSuccess("Command completed successfully.");
     }
 }
